c65c7e8bdc4e5c6c4894a643db798b76d24d9493
sakschmidt <sarah.schmidt@code.seat>
2024-05-18 12:34:20 +0200

    feat: correct test titles

--- cypress/e2e/submodule-list.spec.cy.ts
+++ cypress/e2e/submodule-list.spec.cy.ts
@@ -15,12 +15,12 @@ describe('Submodule-list', () => {
     cy.findByRole('link', { name: /empleados/i }).should('be.visible');
   });
 
-  it('should navigate to "/projects" when project link is clicked', () => {
+  it('should navigate to "/projects" when "Proyectos" is clicked', () => {
     cy.findByRole('link', { name: /proyectos/i }).click();
     cy.url().should('contain', 'projects');
   });
 
-  it('should navigate to "/employees" when project link is clicked', () => {
+  it('should navigate to "/employees" when "Empleados" is clicked', () => {
     cy.findByRole('link', { name: /empleados/i }).click();
     cy.url().should('contain', 'employees');
   });

7cdc1bd8c4b89b0bf8b988ad1cc706e5b7b6deba
sakschmidt <sarah.schmidt@code.seat>
2024-05-21 23:02:24 +0200

    feat: add screenshot option when tests failing, add command for e2e on ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080/#',
     specPattern: 'cypress/e2e/**/*.spec.cy.{js,jsx,ts,tsx}',
+    screenshotOnRunFailure: true,
   },
 });
